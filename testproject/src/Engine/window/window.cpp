b25836826d844ed134b20af24c23fdc875b6b790
unknown3211 <100625853+unknown3211@users.noreply.github.com>
2025-07-15 07:56:25 -0400

    Fixed Duplication Of GL_COLOR_BUFFER_BIT

--- testproject/src/Engine/window/window.cpp
+++ testproject/src/Engine/window/window.cpp
@@ -62,7 +62,7 @@ void Window::Update(void(*update)())
 	{
 		ProcessInput(window);
 		ResizeWindow(window);
-		glClear(GL_COLOR_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+		 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		update();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
